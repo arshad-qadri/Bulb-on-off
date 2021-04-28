63015aa50e31e2cdcb4d4cab997ee9efef4bf865
arshad-qadri <arshadqadri321@gmail.com>
2021-04-28 11:30:30 +0530

    bulb-on-off-uploading

---
+++ index.js
@@ -0,0 +1,17 @@
+const btn = document.getElementById('btn');
+const img1 = document.getElementById('img1');
+const img2 = document.getElementById('img2');
+const bgColor = document.getElementById('container');
+btn.addEventListener('click', () => {
+  if (img1.style.display === 'block' || btn.innerText === 'ON') {
+    img1.style.display = 'none';
+    img2.style.display = 'block';
+    bgColor.style.backgroundColor = '#000';
+    btn.innerHTML = 'OFF';
+  } else {
+    img1.style.display = 'block';
+    img2.style.display = 'none';
+    bgColor.style.backgroundColor = '#fff';
+    btn.innerHTML = 'ON';
+  }
+});
